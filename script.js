8219c35c8a74a7182dede9d87f818b7b99862de3
Erhard Nagy <Erson1989x@gmail.com>
2024-10-19 14:58:17 +0300

    progress

--- script.js
+++ script.js
@@ -283,7 +283,7 @@ wait(1).then(() => {
 
 Promise.resolve('abc').then(x => console.log(x))
 Promise.reject(new Error('Problem')).catch(x => console.log(x))
-*/
+
 
 
 const getPosition = () => {
@@ -329,3 +329,57 @@ const getPosition = () => {
   }
 
   btn.addEventListener('click', whereAmI)
+
+
+const imgContainer = document.querySelector('.images');
+
+const createImage = (imgPath) => {
+  return new Promise((resolve, reject) => {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', () => {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', () => {
+      reject(new Error('Image not found'));
+    });
+  });
+}
+
+const wait = (seconds) => {
+  return new Promise(resolve => {
+    setTimeout(resolve, seconds * 1000);
+  })
+}
+
+let currentImg;
+
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    console.log('Image 1 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 2 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-3.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 3 loaded');
+    return wait(2);
+  })
+  .catch(err => console.error(err));
+*/
